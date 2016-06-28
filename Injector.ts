81b5457db2be039f4834a5632b25e102190049cf
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-06-28 11:31:18 +0900

    [add] Injector

---
+++ Injector.ts
@@ -0,0 +1,39 @@
+import { PropTypes, ReactPropTypes } from "react";
+
+class Provider {}
+
+export interface Providers {
+  [key: string]: Provider;
+}
+
+interface PropTypeDictionary {
+  [key: string]: ReactPropTypes;
+}
+
+const assignPropTypes = (acc: PropTypeDictionary, token: string) => Object.assign({}, acc, { [token]: PropTypes.object });
+
+export class Injector {
+  private tokens: string[];
+  childContextTypes: PropTypeDictionary;
+
+  constructor(private providers: Providers) {
+    this.tokens = Object.keys(providers);
+    this.childContextTypes = this.tokens.reduce(assignPropTypes, {});
+  }
+
+  getChildContext() {
+    return this.providers;
+  }
+}
+
+/** サンプルのクラス実装 */
+export class MyService implements Provider {
+  private serviceName: string = "Injectable service.";
+  getName() {
+    return this.serviceName;
+  }
+}
+
+export enum ProviderTokens {
+  MyService,
+}
--- app.tsx
+++ app.tsx
@@ -1,28 +1,46 @@
 import * as React from "react";
-import { StatelessComponent } from "react";
-
+import { Component, StatelessComponent, PropTypes, ReactElement, ValidationMap } from "react";
 import { render } from "react-dom";
 
+import { Injector, MyService, Providers, ProviderTokens } from "./Injector";
+
+/** Child */
 interface ChildProps {
   name: string;
   count: number;
 }
 
-/** 状態を持たないコンポーネントの例 */
-const MyChildComponent: StatelessComponent<ChildProps> = (props) => <div>{ props.count } / { props.name }</div>;
+/** 状態を持たないコンポーネント */
+const MyChildComponent: StatelessComponent<ChildProps> = (props, _context) => {
+  const context = _context as Providers;
+  const myService = context[ProviderTokens.MyService] as MyService;
+  return (<div>{ props.count } / { props.name } / { myService.getName() }</div>);
+};
 
-/*
-ReactComponentClass
-class MyChildComponent extends React.Component<ChildProps, any> {
+MyChildComponent.contextTypes = {
+  [ProviderTokens.MyService]: PropTypes.object
+};
+
+
+/** App */
+const inject = new Injector({
+  [ProviderTokens.MyService]: new MyService(),
+});
+
+class App extends Component<void, void> {
+  static childContextTypes = inject.childContextTypes;
+
+  getChildContext() {
+    return inject.getChildContext();
+  }
+
   render() {
-    return <div>{ this.props.count * 2 } / { this.props.name + "." }</div>;
+    return (
+      <section>
+        <MyChildComponent name="myname" count={ 1 }></MyChildComponent>
+      </section>
+    );
   }
 }
-*/
 
-const App: StatelessComponent<void> = () => (
-  <section>
-    <h1>Foo</h1>
-    <MyChildComponent name="myname" count={ 1 }></MyChildComponent>
-  </section>
-);
+render(<App></App>, document.getElementById("root"));
